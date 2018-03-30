7aaaf4cb85eb8d3e64dc7241d20ef1f922d670e2
Bulat <sadikovba@gmail.com>
2018-03-30 18:33:14 +0300

    Header

--- index.php
+++ index.php
@@ -12,24 +12,24 @@
 					<a href="#">Контакты</a>
 				</nav>
 				<div class="header__top-contact">
-					<a href="tel:88002000574" class="header__top-phone"><i></i><span>8 800 200-05-74</span></a>
+					<a href="tel:88002000574" class="header__top-phone"><i class="i-phone"></i><span>8 800 200-05-74</span></a>
 					<a href="#" class="header__top-call fancy" data-fancybox="#modalcall">Обратный звонок</a>
-					<a href="#" class="header__cabinet"><i></i><span>Личный кабинет</span></a>
+					<a href="#" class="header__cabinet"><i class="i-person"></i><span>Личный кабинет</span></a>
 				</div>
 			</div>
 		</div>
-		<div class="header__middle">
+		<div class="header__middle container">
 			<a href="#" class="header__logo">
 				<img src="img/logo-top.svg" alt="3D System Pro">
 			</a>
-			<form>
-				<input type="text" placeholder="Поиск">
-				<button><i></i></button>
+			<form class="form-search">
+				<input type="text" class="input-text input-text--search" placeholder="Поиск">
+				<button><i class="i-search"></i></button>
 			</form>
 			<div class="header__cart-wrap">
-				<a href="#" class="header__compare"><i><sup>2</sup></i></a>
+				<a href="#" class="header__compare"><i class="i-compare"><sup>52</sup></i></a>
 				<a href="#" class="header__cart">
-					<i><sup>3</sup></i>
+					<i class="i-card"><sup>3</sup></i>
 					<span class="header__cart-info">
 						<b>Корзина: 3 товара</b>
 						<span>на сумму 135 000 a</span>
@@ -37,7 +37,7 @@
 				</a>
 			</div>
 		</div>
-		<div class="header__bottom">
+		<div class="header__bottom container">
 			<ul>
 				<li><a href="#">Каталог</a></li>
 				<li><a href="#">3D принтеры</a></li>
