63a9308aeccfe40dfca8b5af44f24981e4d15246
Bulat <sadikovba@gmail.com>
2018-04-11 11:42:01 +0300

    Cart Page

---
+++ cart.php
@@ -0,0 +1,154 @@
+<?php include('header.php'); ?>
+
+<section class="s-page-title">
+	<div class="container">
+		<h1 class="h1">Корзина</h1>
+		<div class="user-cart__order-action user-cart__order-action--head">
+			<a href="#" class="btn btn--blue">Продолжить покупки</a>
+			<a href="#" class="btn btn--red">Оформить заказ</a>
+		</div>
+	</div>
+</section>
+
+<section class="user-cart-sec s-def-bottom">
+	<div class="user-cart container">
+		<div class="row">
+			<div class="grid-8 grid-12_l">
+				<div class="user-cart__list">
+					<div class="user-cart__item">
+						<div class="user-cart__item-img">
+							<img src="img/preview/pr_1.jpg" alt="">
+						</div>
+						<div class="user-cart__item-info">
+							<div class="user-cart__item-name">
+								<span class="user-cart__item-title">Описание</span>
+								<a href="#" class="user-cart__item-value h3">3D ринтер PICASO DESIGNER X PRO</a>
+								<span class="user-cart__item-title user-cart__item-title--code">Код товара: #1119215</span>
+							</div>
+							<div class="user-cart__item-price">
+								<span class="user-cart__item-title">Цена</span>
+								<span class="user-cart__item-value h3">279 000 <span class="rub">c</span></span>
+							</div>
+							<div class="user-cart__item-count">
+								<span class="user-cart__item-title">Количество</span>
+								<span class="user-cart__item-value">
+									<form>
+									<button>-</button>
+									<input type="text" value="1">
+									<button>+</button>
+								</form>
+								</span>
+
+							</div>
+							<div class="user-cart__item-summ">
+								<span class="user-cart__item-title">Сумма</span>
+								<span class="user-cart__item-value h3">279 000 <span class="rub">c</span></span>
+							</div>
+						</div>
+						<a href="#" class="user-cart__item-remove" title="Удалить товар"><i class="i-remove "></i></a>
+					</div>
+					<div class="user-cart__item">
+						<div class="user-cart__item-img">
+							<img src="img/preview/pr_2.jpg" alt="">
+						</div>
+						<div class="user-cart__item-info">
+							<div class="user-cart__item-name">
+								<span class="user-cart__item-title">Описание</span>
+								<a href="#" class="user-cart__item-value h3">3D ринтер PICASO DESIGNER X PRO</a>
+								<span class="user-cart__item-title user-cart__item-title--code">Код товара: #1119215</span>
+							</div>
+							<div class="user-cart__item-price">
+								<span class="user-cart__item-title">Цена</span>
+								<span class="user-cart__item-value h3">279 000 <span class="rub">c</span></span>
+							</div>
+							<div class="user-cart__item-count">
+								<span class="user-cart__item-title">Количество</span>
+								<span class="user-cart__item-value">
+									<form>
+									<button>-</button>
+									<input type="text" value="1">
+									<button>+</button>
+								</form>
+								</span>
+
+							</div>
+							<div class="user-cart__item-summ">
+								<span class="user-cart__item-title">Сумма</span>
+								<span class="user-cart__item-value h3">279 000 <span class="rub">c</span></span>
+							</div>
+						</div>
+						<a href="#" class="user-cart__item-remove" title="Удалить товар"><i class="i-remove "></i></a>
+					</div>
+					<div class="user-cart__item">
+						<div class="user-cart__item-img">
+							<img src="img/preview/pr_3.jpg" alt="">
+						</div>
+						<div class="user-cart__item-info">
+							<div class="user-cart__item-name">
+								<span class="user-cart__item-title">Описание</span>
+								<a href="#" class="user-cart__item-value h3">3D ринтер PICASO DESIGNER X PRO</a>
+								<span class="user-cart__item-title user-cart__item-title--code">Код товара: #1119215</span>
+							</div>
+							<div class="user-cart__item-price">
+								<span class="user-cart__item-title">Цена</span>
+								<span class="user-cart__item-value h3">279 000 <span class="rub">c</span></span>
+							</div>
+							<div class="user-cart__item-count">
+								<span class="user-cart__item-title">Количество</span>
+								<span class="user-cart__item-value">
+									<form>
+									<button>-</button>
+									<input type="text" value="1">
+									<button>+</button>
+								</form>
+								</span>
+
+							</div>
+							<div class="user-cart__item-summ">
+								<span class="user-cart__item-title">Сумма</span>
+								<span class="user-cart__item-value h3">279 000 <span class="rub">c</span></span>
+							</div>
+						</div>
+						<a href="#" class="user-cart__item-remove" title="Удалить товар"><i class="i-remove "></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="grid-4 grid-12_l">
+				<div class="user-cart__order">
+					<p class="h2">Ваш заказ</p>
+					<div class="user-cart__order-item">
+						<span class="user-cart__order-title">Всего товаров</span>
+						<span class="user-cart__order-value">3 шт.</span>
+					</div>
+					<div class="user-cart__order-item">
+						<span class="user-cart__order-title">Сумма</span>
+						<span class="user-cart__order-value">822 000 <span class="rub">a</span></span>
+					</div>
+					<div class="user-cart__order-item">
+						<span class="user-cart__order-title">Скидка</span>
+						<span class="user-cart__order-value">15 <span class="rub">a</span></span>
+					</div>
+					<div class="user-cart__order-promo">
+						<span>Хотите скидку?</span>
+						<div class="user-cart__order-form">
+							<input type="text" class="input-text" placeholder="Введите промокод">
+							<a href="#" class="btn btn--blue"><i class="i-reload"></i></a>
+						</div>
+					</div>
+					<div class="user-cart__order-item user-cart__order-item--bold">
+						<span class="user-cart__order-title">К оплате</span>
+						<span class="user-cart__order-value">822 000 <span class="rub">c</span></span>
+					</div>
+					<div class="user-cart__order-action">
+						<a href="#" class="btn btn--blue">Продолжить покупки</a>
+						<a href="#" class="btn btn--red">Оформить заказ</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
