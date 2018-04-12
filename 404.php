b9dce876bed881ce8e5ffdc047a4c8179037f39d
Andrew <andreiduffy@gmail.com>
2018-04-12 14:34:46 +0300

    404

---
+++ 404.php
@@ -0,0 +1,18 @@
+﻿<?php include('header.php'); ?>
+
+
+<section class="s-404 s-def-bottom">
+	<div class="container user-content">
+		<img src="/img/404.jpg" alt="404">
+		<p class="h1">Хьюстон, у нас проблемы</p>
+		<p>Страница, на которую вы пытаетесь попасть, не существует или была удалена. Перейдите:</p>
+		<a href="/">Главная страница</a>
+		<br><br>
+		<a href="/catalog.php">Каталог</a>
+		<br><br>
+	</div>
+</section>
+
+
+
+<?php include('footer.php'); ?>
