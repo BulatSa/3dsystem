ffaaa93bce5300e07ad4cfc96c630faf946868bb
Andrew <andreiduffy@gmail.com>
2018-04-13 13:09:24 +0300

    blog

--- blog-detail.php
+++ blog-detail.php
@@ -14,16 +14,28 @@
 </section>
 
 
+<section class="s-news-detail s-news-detail--video">
+	<div class="container">
+		<div class="user-content">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/BWbFYZTrpM4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</div>
+	</div>
+</section>
+
+
 <section class="s-news-detail s-def-bottom">
 	<div class="container">
 
 		<div class="user-content">
-			<!--detail-picture-->
-			<img src="/img/preview/promo-img.jpg" alt="111" title="Участвуют в акции: ABS, PLA, PLA PRO, HIPS, NYLON">
-			<!--detail-picture-->
-			<p>Доброго дня!</p>
 			<p>Недавно к нам в руки попал новенький 3D принтер Printbox3D 270 PRO от одноименной компании. После многодневной экзекуции эксплуатации мы решили написать о нем красочный обзор, с картинами, подробным описанием каждого узла, с плюсами и минусами (на наш взгляд).</p>
 			<p>Итак, не будем надолго затягивать приветствие и сразу перейдем к обзору.</p>
+			<img src="/img/preview/news-img.jpg" alt="111" title="3D принтера PrintBox3D 270 PRO">
+			<p>Принтер построен по кинематике Head X Bed YZ, то есть экструдер движется по оси X, а стол по осям Y и Z. Такую кинематику любители 3D принтеров ласково зовут «дрыгостол». Можно с легкостью провести аналогию в кинематике с предшественником 270 PRO – принтером Printbox3D One. Главное различие – это область печати и габариты устройства. Экструдеры так же очень похожи.</p>
+			<p>Итак, не будем надолго затягивать приветствие и сразу перейдем к обзору.</p>
+			<img src="/img/preview/news-img.jpg" alt="111" title="3D принтера PrintBox3D 270 PRO">
+			<p>270 PRO комплектуется подогреваемым рабочим столом и одним экструдером под пластик 1,75мм. Экструдер директ, то есть шаговый двигатель и подающий механизм находятся непосредственно в экструдере, что позволяет печать абсолютным большинством пластиков, включая эластичные. Область печати 270*250*240мм (X*Y*Z). Максимальная температура сопла 270С, стола 130С. Принтер комплектуется соплом 0.4мм. Возможные размеры сопел: от 0.2 до 0.5</p>
+			<h3>Упаковка и комплектация</h3>
+			<p>Упакован принтер в деревянный короб размером 620* 575*640мм и весом 36кг. От ударов при перевозке принтер оберегает проложенный по кругу вспененный полиэтилен толщиной 50мм. Общее ощущение от упаковки крайне положительное! На наш взгляд, при перевозке принтер может пострадать только от падения метров с пяти, или от непосредственной близости к месту ядерного удара.</p>
 			<p>Комплектация радует, есть все, включая инструменты для мелкого ремонта и юстировки:</p>
 			<ul>
 				<li>кабель USB</li>
@@ -44,6 +56,9 @@
 				<li>трубка фторопласт 50 см</li>
 				<li>катушка пластика CyberFiber</li>
 			</ul>
+			<h3>Первый взгляд, общие черты</h3>
+			<p>Принтер лучше доставать из коробки и ставить на стол вдвоем, так как он реально тяжелый и массивный. Мы поставили его на стол, подключили к сети 220В, нажали кнопку питания. Стоит заметить ,что в принтере есть подсветка области печати а самое главное что она отключается! Для этого спереди есть кнопка. Это спасение для тех кто много находится рядом с принтером во время печати – лишний свет иногда раздражает и бьет в глаза. Так что это несомненный плюс в копилку 270 PRO.</p>
+			<img src="/img/preview/news-img.jpg" alt="111" title="3D принтера PrintBox3D 270 PRO">
 		</div>
 
 		<div class="share-block">
@@ -58,7 +73,7 @@
 			</div>
 		</div>
 
-		<a href="/promo.php" class="btn">К списку акций</a>
+		<a href="/blog.php" class="btn">К списку статей</a>
 
 	</div>
 </section>
@@ -68,100 +83,103 @@
 	<div class="container">
 
 		<div class="section-title">
-			<p class="h1"><a href="/promo.php">Последние записи</a></p>
+			<p class="h1"><a href="/blog.phpp">Последние записи</a></p>
 			<div class="section-title__actions">
 				<a href="/blog.php" class="btn btn--small">Вернуться в блог</a>
 			</div>
 		</div>
 
-		<div class="row home-promos">
+		<div class="row preview-wall">
 			<div class="grid-2 grid-4_l grid-6_s">
 				<div class="preview">
-					<a href="#">
+					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fancy-video preview__video" title="Посмотреть видео">
+						<img src="/img/home/blog-1.jpg" alt="243x194" class="preview__img">
+						<span class="play-icon"></span>
+					</a>
+					<div class="preview__content">
+						<p class="preview__date small-text">22.03.2018</p>
+						<p class="h3 preview__title"><a href="/blog-detail.php">Пятая катушка пластика в подарок!</a></p>
+						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
+					</div>
+					<footer class="preview__footer">
+						<a href="/blog-detail.php" class="btn btn--small">Подробнее</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-2 grid-4_l grid-6_s">
+				<div class="preview">
+					<a href="/blog-detail.php">
+						<img src="/img/home/promo-1.jpg" alt="243x194" class="preview__img">
+					</a>
+					<div class="preview__content">
+						<p class="preview__date small-text">22.03.2018</p>
+						<p class="h3 preview__title"><a href="/blog-detail.php">Пятая катушка пластика в подарок!</a></p>
+						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
+					</div>
+					<footer class="preview__footer">
+						<a href="/blog-detail.php" class="btn btn--small">Подробнее</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-2 grid-4_l grid-6_s">
+				<div class="preview">
+					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fancy-video preview__video" title="Посмотреть видео">
+						<img src="/img/preview/243x194.png" alt="243x194" class="preview__img">
+						<span class="play-icon"></span>
+					</a>
+					<div class="preview__content">
+						<p class="preview__date small-text">22.03.2018</p>
+						<p class="h3 preview__title"><a href="/blog-detail.php">Пятая катушка пластика в подарок!</a></p>
+						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
+					</div>
+					<footer class="preview__footer">
+						<a href="/blog-detail.php" class="btn btn--small">Подробнее</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-2 grid-4_l grid-6_s">
+				<div class="preview">
+					<a href="/blog-detail.php">
 						<img src="/img/home/promo.jpg" alt="243x194" class="preview__img">
 					</a>
 					<div class="preview__content">
 						<p class="preview__date small-text">01.03.2018 – 31.04.2018</p>
-						<p class="h3 preview__title"><a href="#">Пятая катушка пластика в подарок!</a></p>
+						<p class="h3 preview__title"><a href="/blog-detail.php">Пятая катушка пластика в подарок!</a></p>
 						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
 					</div>
 					<footer class="preview__footer">
-						<a href="#" class="btn btn--small">Подробнее</a>
+						<a href="/blog-detail.php" class="btn btn--small">Подробнее</a>
 					</footer>
 				</div>
 			</div>
 			<div class="grid-2 grid-4_l grid-6_s">
 				<div class="preview">
-					<a href="#">
+					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fancy-video preview__video" title="Посмотреть видео">
+						<img src="/img/home/blog-1.jpg" alt="243x194" class="preview__img">
+						<span class="play-icon"></span>
+					</a>
+					<div class="preview__content">
+						<p class="preview__date small-text">22.03.2018</p>
+						<p class="h3 preview__title"><a href="/blog-detail.php">Пятая катушка пластика в подарок!</a></p>
+						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
+					</div>
+					<footer class="preview__footer">
+						<a href="/blog-detail.php" class="btn btn--small">Подробнее</a>
+					</footer>
+				</div>
+			</div>
+			<div class="grid-2 grid-4_l grid-6_s">
+				<div class="preview">
+					<a href="/blog-detail.php">
 						<img src="/img/home/promo-1.jpg" alt="243x194" class="preview__img">
 					</a>
 					<div class="preview__content">
-						<p class="preview__date small-text">Весь апрель!</p>
-						<p class="h3 preview__title"><a href="#">Пятая катушка пластика в подарок!</a></p>
-						<p class="small-text">Закажи 4 катушки любого пластика</p>
-					</div>
-					<footer class="preview__footer">
-						<a href="#" class="btn btn--small">Подробнее</a>
-					</footer>
-				</div>
-			</div>
-			<div class="grid-2 grid-4_l grid-6_s">
-				<div class="preview">
-					<a href="#">
-						<img src="/img/home/promo.jpg" alt="243x194" class="preview__img">
-					</a>
-					<div class="preview__content">
-						<p class="preview__date small-text">Только сегодня!</p>
-						<p class="h3 preview__title"><a href="#">Пятая катушка пластика в подарок!</a></p>
+						<p class="preview__date small-text">22.03.2018</p>
+						<p class="h3 preview__title"><a href="/blog-detail.php">Пятая катушка пластика в подарок!</a></p>
 						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
 					</div>
 					<footer class="preview__footer">
-						<a href="#" class="btn btn--small">Подробнее</a>
-					</footer>
-				</div>
-			</div>
-			<div class="grid-2 grid-4_l grid-6_s">
-				<div class="preview">
-					<a href="#">
-						<img src="/img/home/promo-1.jpg" alt="243x194" class="preview__img">
-					</a>
-					<div class="preview__content">
-						<p class="preview__date small-text">Весь апрель!</p>
-						<p class="h3 preview__title"><a href="#">Пятая катушка пластика в подарок!</a></p>
-						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
-					</div>
-					<footer class="preview__footer">
-						<a href="#" class="btn btn--small">Подробнее</a>
-					</footer>
-				</div>
-			</div>
-			<div class="grid-2 grid-4_l grid-6_s">
-				<div class="preview">
-					<a href="#">
-						<img src="/img/preview/243x194.png" alt="243x194" class="preview__img">
-					</a>
-					<div class="preview__content">
-						<p class="preview__date small-text">Весь апрель!</p>
-						<p class="h3 preview__title"><a href="#">Пятая катушка</a></p>
-						<p class="small-text">Закажи 4 катушки любого пластика</p>
-					</div>
-					<footer class="preview__footer">
-						<a href="#" class="btn btn--small">Подробнее</a>
-					</footer>
-				</div>
-			</div>
-			<div class="grid-2 grid-4_l grid-6_s">
-				<div class="preview">
-					<a href="#">
-						<img src="/img/home/promo.jpg" alt="243x194" class="preview__img">
-					</a>
-					<div class="preview__content">
-						<p class="preview__date small-text">Весь апрель!</p>
-						<p class="h3 preview__title"><a href="#">Пятая катушка пластика в подарок!</a></p>
-						<p class="small-text">Закажи 4 катушки любого пластика CyberFiber и получи пятую в подарок!</p>
-					</div>
-					<footer class="preview__footer">
-						<a href="#" class="btn btn--small">Подробнее</a>
+						<a href="/blog-detail.php" class="btn btn--small">Подробнее</a>
 					</footer>
 				</div>
 			</div>
