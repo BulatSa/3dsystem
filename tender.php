bb538a54311bc4a10846d13f05a2efb4577ca907
Andrew <andreiduffy@gmail.com>
2018-07-18 17:42:16 +0300

    tender begin

---
+++ tender.php
@@ -0,0 +1,498 @@
+<?php include('header.php'); ?>
+
+
+<section class="big-banner-sec s-def-bottom">
+	<div class="container">
+		<div class="big-banner big-banner--mini" style="background-image: url('../img/about/bg-big-banner.jpg')">
+			<div class="big-banner__info">
+				<div class="user-content">
+					<p class="h1">Доверие государственных бюджетных организаций</p>
+					<p class="h3">участие в тендерах и государственных закупках 3D-техники</p>
+				</div>
+				<button class="scrollto btn">Сделать заказ</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="section-title s-def-top">
+	<div class="container">
+		<p class="h1">Наши постоянные клиенты</p>
+	</div>
+</section>
+
+
+<section class="about-serts-sec s-def-bottom">
+	<div class="container">
+		<div class="about-serts row">
+			<div class="grid-3 grid-4_m grid-6_xs">
+				<a href="img/tender/logo_1.png" class="about-serts__one fancy">
+					<span>
+						<img src="img/tender/logo_1.png" alt="">
+					</span>
+					<p>Подробнее</p>
+				</a>
+			</div>
+			<div class="grid-3 grid-4_m grid-6_xs">
+				<a href="img/logos/1.png" class="about-serts__one fancy">
+					<span>
+						<img src="img/tender/logo_2.png" alt="">
+					</span>
+					<p>Подробнее</p>
+				</a>
+			</div>
+			<div class="grid-3 grid-4_m grid-6_xs">
+				<a href="img/logos/1.png" class="about-serts__one fancy">
+					<span>
+						<img src="img/tender/logo_3.png" alt="">
+					</span>
+					<p>Подробнее</p>
+				</a>
+			</div>
+			<div class="grid-3 grid-4_m grid-6_xs">
+				<a href="img/logos/1.png" class="about-serts__one fancy">
+					<span>
+						<img src="img/tender/logo_4.png" alt="">
+					</span>
+					<p>Подробнее</p>
+				</a>
+			</div>
+			<div class="grid-3 grid-4_m grid-6_xs">
+				<a href="img/logos/1.png" class="about-serts__one fancy">
+					<span>
+						<img src="img/tender/logo_5.png" alt="">
+					</span>
+					<p>Подробнее</p>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="info-banner-sec s-def-bottom">
+	<div class="container">
+		<div class="info-banner" style="background-image: url('../img/about/bg-info-banner.jpg')">
+			<div class="info-banner__text">
+				<div class="user-content">
+					<p class="h1">Большой опыт участия в&nbsp;тендерах</p>
+					<p>Компания 3Dsystem.pro неоднократно принимала участие в тендерах на поставку 3D-оборудования для государственных организаций. Мы знаем всю процедуру оформления конкурсной документации и заполнения требуемых  спецификаций. 3Dsystem.pro предложит вам выгодные цены на 3D-принтеры и сопутствующие товары, а наши консультанты помогут разобраться в процедуре проведения тендера и ответят на возникающие вопросы. </p>
+					<p>Оборудование от компании 3Dsystem.pro уже установлено в крупных муниципальных учреждениях, в том числе в Национальном Исследовательском Томском Государственном Университете, Волгоградском Государственном Техническом Университете, в Управлении Образования Альметьевского Муниципального Района Республики Татарстан — и это лишь малая часть выигранных нами тендеров.</p>
+				</div>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="info-banner__video fancy-video" title="Посмотреть видео">
+					<span class="play-icon"></span>
+					<p>
+						<span class="h3">Посмотреть видео</span>
+						Длительность 1 минута
+					</p>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="section-title s-def-top">
+	<div class="container">
+		<h1>Удобное сотрудничество</h1>
+	</div>
+</section>
+
+
+<section class="about-benefits-sec">
+	<div class="container">
+		<div class="about-benefits row">
+			<div class="grid-3 grid-6_l grid-12_xs">
+				<div class="about-benefits__item">
+					<div class="about-benefits__img">
+						<img src="img/tender/icon_1.svg" alt="">
+					</div>
+					<div class="about-benefits__text">
+						<p class="h3">Большой выбор 3D-оборудования</p>
+						<span>Техника брендов, зарекомендовавших себя на рынке 3D-оборудования во всем мире.</span>
+					</div>
+				</div>
+			</div>
+			<div class="grid-3 grid-6_l grid-12_xs">
+				<div class="about-benefits__item">
+					<div class="about-benefits__img">
+						<img src="img/tender/icon_2.svg" alt="">
+					</div>
+					<div class="about-benefits__text">
+						<p class="h3">Поддержка и сервис в любое время</p>
+						<span>Гарантийный ремонт техники брендов  CYBERON и IMPRINTA. Постгарантийное обслуживание 3D-принтеров и сопутствующего оборудования разных производителей.</span>
+					</div>
+				</div>
+			</div>
+			<div class="grid-3 grid-6_l grid-12_xs">
+				<div class="about-benefits__item">
+					<div class="about-benefits__img">
+						<img src="img/tender/icon_3.svg" alt="">
+					</div>
+					<div class="about-benefits__text">
+						<p class="h3">Опыт участия в аукционах и  тендерах</p>
+						<span>3Dsystem.pro — надежный поставщик оборудования с опытом участия в тендерах на самом высоком уровне и выгодными условиями для сотрудничества.</span>
+					</div>
+				</div>
+			</div>
+			<div class="grid-3 grid-6_l grid-12_xs">
+				<div class="about-benefits__item">
+					<div class="about-benefits__img">
+						<img src="img/tender/icon_4.svg" alt="">
+					</div>
+					<div class="about-benefits__text">
+						<p class="h3">Соблюдение сроков поставки оборудования</p>
+						<span>Только проверенные транспортные компании, гарантирующие доставку точно в срок. </span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="history-banner-sec s-def">
+	<div class="container">
+		<div class="history-banner">
+			<div id="particles-js" data-json="/img/particles.json"></div>
+			<div class="history-banner__text">
+				<p class="h2">С 2012 года работаем для вас</p>
+				<p class="h3">Начните сотрудничество с нами прямо сейчас!</p>
+				<button class="btn btn--red fancy-modal" data-src="#modal-partner">Стать партнером</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-logos-slider">
+	<div class="container">
+
+		<div class="logos-slider">
+			<div class="logos-slider__block">
+				<p class="h2">Мы официальные представители</p>
+				<p class="small-text">Только официальные представители могут давать гарантии на&nbsp;оборудование и&nbsp;предоставлять низкие цены от&nbsp;производителя.</p>
+				<div class="logos-slider__nav">
+					<button class="logos-slider__prev"><i class="i-left-arrow"></i></button>
+					<button class="logos-slider__next"><i class="i-right-arrow"></i></button>
+				</div>
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/1.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/2.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/3.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/4.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/5.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/1.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/2.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/3.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/4.png" alt="logo">
+			</div>
+			<div class="logos-slider__item">
+				<img src="/img/logos/5.png" alt="logo">
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+<section class="section-title s-def-top">
+	<div class="container">
+		<h1>Сертификаты</h1>
+	</div>
+</section>
+<section class="mails-sec s-def-bottom">
+	<div class="mails container">
+		<div class="mails__list">
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" data-fancybox>
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" data-fancybox>
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" data-fancybox>
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mails__nav">
+			<span class="mails__nav-prev"><i class="i-left"></i></span>
+			<span class="mails__nav-next"><i class="i-right"></i></span>
+		</div>
+	</div>
+</section>
+
+
+<section class="section-title s-def-top">
+	<div class="container">
+		<h1>Благодарственные письма</h1>
+	</div>
+</section>
+<section class="mails-sec s-def-bottom">
+	<div class="mails container">
+		<div class="mails__list">
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" data-fancybox>
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" data-fancybox>
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" data-fancybox>
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+			<div class="mails__item-wrap grid-2 grid-3_l grid-4_s grid-6_xs grid-12_xxs">
+				<div class="mails__item">
+					<div class="mails__item-img">
+						<a href="img/mails/mail.jpg" class="fancy">
+							<img src="img/mails/mail.jpg" alt="">
+						</a>
+					</div>
+					<div class="mails__item-text">
+						<span>Министерство  по делам молодежи, физической культуры и спорта Омской области</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mails__nav">
+			<span class="mails__nav-prev"><i class="i-left"></i></span>
+			<span class="mails__nav-next"><i class="i-right"></i></span>
+		</div>
+	</div>
+</section>
+
+
+<section class="feedback-sec s-def">
+	<div class="container">
+		<div class="feedback">
+			<p class="h2">Сделать заказ</p>
+			<p class="feedback__subtitle">Заполните форму — для рассмотрения Вашего запроса.</p>
+			<div class="feedback__form">
+				<form class="ajax-form">
+					<input type="hidden" value="Заявка на тендер" name="form_subject">
+					<div class="feedback__input">
+						<input type="text" name="user_name" placeholder="Введите имя*" data-label="Имя пользователя*" class="input-text" data-req="true">
+					</div>
+					<div class="feedback__input">
+						<input type="tel" name="user_tel" placeholder="Введите телефон*" data-label="Телефон"  class="input-text" data-req="true">
+					</div>
+					<div class="feedback__input">
+						<input type="email" name="user_email" placeholder="Введите e-mail*" data-label="E-mail*" class="input-text" data-req="true">
+					</div>
+					<div class="feedback__input feedback__input--full">
+						<textarea name="user_comment" placeholder="Детали заявки" class="input-text" data-label="Детали заявки"></textarea>
+					</div>
+					<div class="feedback__input feedback__input--full">
+						<div class="form-file form-file--half">
+							<input type="hidden" name="MAX_FILE_SIZE" value="10000000">
+							<div class="row">
+								<div class="grid-6 grid-12_s">
+									<span class="form-file__title">Загрузите файл проекта<br><strong>(до 10 Мбайт)</strong></span>
+									<label class="style-file">
+										<input type="file" name="user_file" data-multiple-caption="{count} файла(ов)" multiple>
+										<span class="btn">Выбрать файл</span>
+										<span class="style-file__text"></span>
+									</label>
+								</div>
+								<div class="grid-6 grid-12_s">
+									<span class="form-file__title">Если ваш файл <strong>больше 10 Мбайт</strong>, то укажите ссылку на его скачивание (Яндекс.Диск, Google.Drive и т.п.)</span>
+									<input type="text" name="user_filelink" placeholder="Ссылка на файл" data-label="Ссылка на файл" class="input-text">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="feedback__submit">
+						<label class="style-check-ios">
+							<input type="checkbox" name="user_policy" data-label="Согласен с условиями" value="yes" data-req="true" checked="">
+							<span>Даю свое согласие на&nbsp;обработку моих персональных данных и&nbsp;соглашаюсь с&nbsp;<a href="/privacy.php" target="_blank">политикой конфиденциальности</a></span>
+						</label>
+						<button class="btn btn--blue">Отправить </button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
